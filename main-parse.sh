24eb1947cfb807ae5bc6c8ec904aef52a1977987
USERMAN7 <tokdlybrawla@gmail.com>
2025-09-09 21:43:08 +0200

    added algebra

--- main-parse.sh
+++ main-parse.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
 geometry="https://kzgdz.com/8-class/geometry-shinibekob-8-2018/u23-"
-echo "BETA, only supported Geometry 8 grade."
-echo "For example if you're looking for exercise 2.30"
-echo "input number of the exercise:"
+algebra="https://kzgdz.com/8-class/algebra-shinibekov-8-2018/u29-"
+echo "BETA only supported 8 grade. algebra,geometry"
+printf "Input the name of book:"
+read -r book
+if [ -n $book ]; then
+	if [ $book == "algebra" ]; then
+		book="$algebra"
+		bookn="Algebra"
+	elif [ $book == "geometry" ]; then
+		book="$geometry"
+		bookn="Geometry"
+	fi
+fi
+printf "input number of the exercise:"
 read -r ex
-ex="${ex//./-}"
+#ex="${ex//./-}"
 echo "downloading from https://kzgdz.com/8-class/geometry-shinibekob-8-2018/u23-$ex"
-wget -O "$ex-tmp" "$geometry$ex"|| { echo "failed to download!"; exit 1; }
+wget -O "$ex-tmp" "$book-$ex"|| { echo "failed to download!"; exit 1; }
 url=$(cat "$ex"-tmp|grep imgs.kzgdz.com|gawk -F'"' '{print $6}')
 rm "$ex-tmp"
-wget -O "Geometry-$ex.jpg" "$url" || { echo "failed to download!"; exit 1; }
+wget -O "$bookn-$ex.jpg" "$url" || { echo "failed to download!"; exit 1; }
