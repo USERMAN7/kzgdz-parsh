6f6bfe5285a1bc1cf20c0ea7ba6195880cde9325
USERMAN7 <userman77777@proton.me>
2025-09-11 04:06:10 +0000

    added a littlw more verbose   main-parse.sh

--- main-parse.sh
+++ main-parse.sh
@@ -17,9 +17,9 @@ fi
 printf "\ninput number of the exercise:"
 read -r ex
 ex="${ex//./-}"
-echo "downloading from https://kzgdz.com/8-class/geometry-shinibekob-8-2018/u23-$ex"
+echo "downloading from $book$ex"
 wget -O "$ex-tmp" "$book-$ex" &>/dev/null || { echo "failed to download!"; exit 1; }
-url=$(cat "$ex"-tmp|grep imgs.kzgdz.com|gawk -F'"' '{print $6}')
+url=$(cat "$ex"-tmp|grep imgs.kzgdz.com|awk -F'"' '{print $6}')
 rm "./$ex-tmp"
 wget -O "$bookn-$ex.jpg" "$url" &>/dev/null|| { echo "failed to download!"; exit 1; }
 echo "saved $bookn-$ex.jpg"
