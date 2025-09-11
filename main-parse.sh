4c50399845c99bec027f4ecb23a35675dd345c26
USERMAN7 <tokdlybrawla@gmail.com>
2025-09-11 19:25:28 +0200

    made spelling a little better

--- main-parse.sh
+++ main-parse.sh
@@ -27,19 +27,23 @@ fi
 printf "input number of the exercise:"
 read -r ex
 ex="${ex//./-}" # converting "." to "."
-echo "downloading from $book$ex"
+echo "Downloading from $book$ex"
 wget -O "$ex-tmp" "$book-$ex" &>/dev/null || { echo "failed to download!"; exit 1; }
 url=$(cat "$ex"-tmp|grep imgs.kzgdz.com|awk -F'"' '{print $6}') 
 url=$(echo "$url" | tr -s '[:space:]' ' ')
 rm "./$ex-tmp" # removing temporary file to not use much space for no reason
 # also deleting in ./ directory because sometime downloading some non existient file can cause - in name which triggers "--help" in rm
-echo found img $url
+echo "Found img $url"
 IFS=' ' read -r -a ur <<< "$url"
 cycle=0
 for img_url in "${ur[@]}"; do
-    wget -O "${bookn}-${ex}-${cycle}.jpg" "$img_url" &>/dev/null|| { echo "failed to download $img_url"; exit 1; }
-    echo "${bookn}-${ex}-${cycle}.jpg was saved"
-    ((cycle++))
+	    if [[ $cycle == 0 ]]; then
+		    wget -O "${bookn}-${ex}.jpg" "$img_url" &>/dev/null|| { echo "failed to download $img_url"; exit 1; }
+    	    else
+	    	wget -O "${bookn}-${ex}-${cycle}.jpg" "$img_url" &>/dev/null|| { echo "failed to download $img_url"; exit 1; }
+	    fi
+	    echo "${bookn}-${ex}-${cycle}.jpg was saved"
+	    ((cycle++))
 done
 #wget -O "$bookn-$ex.jpg" "$url" &>/dev/null|| { echo "failed to download!"; exit 1; } # deleting output to have cleaner look
 #echo "saved $bookn-$ex.jpg" # saying out loud that file was downloaded because there's no output from wgefor url in $urls; do
