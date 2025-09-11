89c6dabb23ae645e78c943973df4a2b17ee3f4f7
USERMAN7 <userman77777@proton.me>
2025-09-11 04:33:16 +0000

    reverse method -z instead of -n   main-parse.sh

--- main-parse.sh
+++ main-parse.sh
@@ -4,28 +4,39 @@ algebra="https://kzgdz.com/8-class/algebra-shinibekov-8-2018/u29-"
 echo "BETA only supported 8 grade. algebra,geometry"
 printf "Input the name of book:"
 read -r book
-book="$(printf $book|tr '[:upper:]' '[:lower:]')"
+if [ -z $book ]; then
+	echo input something
+	exit 1
+fi
+book="$(printf $book|tr '[:upper:]' '[:lower:]')" # converting upper case to lower case
 if [ -n $book ]; then
 	if [ $book == "algebra" ]; then
 		book="$algebra"
-		bookn="Algebra"
+		bookn="Algebra" # adds bookn var to identify which ex was downloaded in the end
 	elif [ $book == "geometry" ]; then
 		book="$geometry"
 		bookn="Geometry"
+	else
+		echo "input geometry or algebra"
+		exit 1
 	fi
+else
+	echo "input book name next time"
+	exit 1
 fi
 printf "input number of the exercise:"
 read -r ex
-ex="${ex//./-}"
+ex="${ex//./-}" # converting "." to "."
 echo "downloading from $book$ex"
 wget -O "$ex-tmp" "$book-$ex" &>/dev/null || { echo "failed to download!"; exit 1; }
 url=$(cat "$ex"-tmp|grep imgs.kzgdz.com|awk -F'"' '{print $6}') 
-rm "./$ex-tmp"
-if [ -n $url ]; then
-	echo "found img "$url" "
+rm "./$ex-tmp" # removing temporary file to not use much space for no reason
+# also deleting in ./ directory because sometime downloading some non existient file can cause - in name which triggers "--help" in rm
+if [ -n $url ]; then # can be empty if some of commands fails
+	echo "found img "$url" " 
 else
 	echo "not found.. exiting"
 	exit 1
 fi
-wget -O "$bookn-$ex.jpg" "$url" &>/dev/null|| { echo "failed to download!"; exit 1; }
-echo "saved $bookn-$ex.jpg"
+wget -O "$bookn-$ex.jpg" "$url" &>/dev/null|| { echo "failed to download!"; exit 1; } # deleting output to have cleaner look
+echo "saved $bookn-$ex.jpg" # saying out loud that file was downloaded because there's no output from wget
